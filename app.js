7085a9e9ab1b2703f8bfccce0a0372abec2b4e0a
刘建文 <lovedreamer7@163.com>
2019-08-28 10:33:51 +0800

    支持多实例、负载均衡、熔断

--- app.js
+++ app.js
@@ -40,37 +40,44 @@ async function regService(app) {
     const { config } = app;
     const { consul: consulConf } = config;
     if (consulConf.register) {
-        await app.consul.agent.service.register(app.config.consul);
+        consulConf.name += config.keys instanceof Array ? config.keys[0] : config.keys;
+        await app.consul.agent.service.register(consulConf);
     }
 }
 /**
  * 服务发现
  * @param {Application} app app
  */
-async function findService(app) {
-    app.services = {};
-    const { services } = app.config.consul;
-    for (const elem of services) {
-        const { referName, serviceId } = elem;
-        // 通过服务id获得ws
-        const checks = await app.consul.agent.check.list();
-        const services = await app.consul.agent.service.list();
-        if (Object.keys(checks).length <= 0) {
-            throw new Error(`找不到该服务:${serviceId}`);
+async function mount(app) {
+    const { services: servicesConf } = app.config.consul;
+    app.services = new Proxy({}, {
+        get: async (target, propKey, receiver) => {
+            const matchedService = servicesConf.find(elem => elem.referName === propKey);
+            if (!matchedService) {
+                return;
+            }
+            let { serviceId } = matchedService;
+            const checks = await app.consul.agent.check.list();
+            const services = await app.consul.agent.service.list();
+            if (Object.keys(checks).length === 0) {
+                return;
+            }
+            const checkId = 'service:' + serviceId;
+            const keys = Object.keys(checks[0]);
+            const targetServices = keys.filter(elem => {
+                return elem.startsWith(checkId) && checks[0][elem].Status === 'passing';
+            });
+            if (targetServices.length === 0) {
+                return;
+            }
+            const rand = Math.floor(Math.random() * targetServices.length);
+            serviceId = targetServices[rand].split(':')[1];
+            const service = services[0][serviceId];
+            const { Address, Port } = service;
+
+            return 'http://' + Address + ':' + Port;
         }
-        const checkId = 'service:' + serviceId;
-        const check = checks[0][checkId];
-        if (!check) {
-            throw new Error(`找不到该服务:${serviceId}`);
-        }
-        if (check.Status !== 'passing') {
-            throw new Error(`服务异常:${serviceId}`);
-        }
-        const service = services[0][serviceId];
-        const { Address, Port } = service;
-        app.services[referName] = 'http://' + Address + ':' + Port;
-    }
-    app.logger.info('app.services', app.services);
+    });
 }
 
 module.exports = app => {
@@ -81,6 +88,6 @@ module.exports = app => {
         // ------------ 服务注册 ------------
         await regService(app);
         // ------------ 服务发现 ------------
-        await findService(app);
+        await mount(app);
     });
 };
